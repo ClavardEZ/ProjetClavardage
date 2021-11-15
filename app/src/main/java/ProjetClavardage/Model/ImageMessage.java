d1b67449e99087095377427f00cce34de29a8c69
Noecv <chauveanpro@gmail.com>
2021-11-15 16:57:19 +0100

    suppression modelio

--- app/src/main/java/ProjetClavardage/Model/ImageMessage.java
+++ app/src/main/java/ProjetClavardage/Model/ImageMessage.java
@@ -1,10 +1,7 @@
 package ProjetClavardage.Model;
 
-import com.modeliosoft.modelio.javadesigner.annotations.objid;
 
-@objid ("1f4981cb-3fa6-4042-a42b-2281d400f1fd")
 public class ImageMessage extends Message {
-    @objid ("8a989211-21f7-4c4c-8539-57c91e059bd0")
     public String file;
 
 }
--- app/src/main/java/ProjetClavardage/Model/UserManager.java
+++ app/src/main/java/ProjetClavardage/Model/UserManager.java
@@ -1,25 +1,17 @@
 package ProjetClavardage.Model;
 
-import com.modeliosoft.modelio.javadesigner.annotations.objid;
-
-@objid ("40154638-b4be-49f6-b07b-0d956aab30f3")
 public class UserManager {
-    @objid ("cba71e0a-0e72-41b7-aed9-fcc00936ddaa")
     public PrivateUser privateUser;
 
-    @objid ("2e55a1d7-fa91-4e10-a4d3-ca6225e3d584")
     public void setUserConnected() {
     }
 
-    @objid ("5ffa5ee3-74d6-4227-8b8e-59335dc7d6bd")
     public void selfConnected() {
     }
 
-    @objid ("4ef50e48-916a-456c-89ee-769b60f9a8c5")
     public void isConnected() {
     }
 
-    @objid ("f45d3ae8-0e84-4e19-9412-39d5da7e046d")
     public void notifyConnected() {
     }
 
