6028f9927f2851987d7bd973441e50ec79536584
MonsieurSinge <momopelloux@gmail.com>
2021-11-16 11:42:37 +0100

    View : updated GUI, added options panel and added padding

--- app/src/main/java/ProjetClavardage/View/Pan.java
+++ app/src/main/java/ProjetClavardage/View/Pan.java
@@ -1,14 +1,27 @@
 package ProjetClavardage.View;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
-import javax.swing.plaf.TabbedPaneUI;
-import javax.tools.Tool;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 public class Pan extends JPanel {
 
     public Pan() {
         super();
+
+        // loading resources
+        BufferedImage logoutImage = null;
+        BufferedImage settingsImage = null;
+        BufferedImage userImage = null;
+        try {
+            logoutImage = ImageIO.read(this.getClass().getResource("/logout.png"));
+            settingsImage = ImageIO.read(this.getClass().getResource("/settings.png"));
+            userImage = ImageIO.read(this.getClass().getResource("/user.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         /* main panel */
         this.setLayout(new GridBagLayout());
@@ -24,6 +37,7 @@ public class Pan extends JPanel {
         this.add(sidePanel, mainGbc);
 
         sidePanel.setLayout(new GridBagLayout());
+        //sidePanel.setBorder(BorderFactory.createEmptyBorder(15, 15, 0, 15));
         GridBagConstraints sideGbc = new GridBagConstraints();
 
         /* options panel */
@@ -31,12 +45,22 @@ public class Pan extends JPanel {
         sideGbc.gridx = 0;
         sideGbc.gridy = 0;
         sideGbc.weightx = 1.0;
-        sideGbc.weighty = 1.0;
+        sideGbc.weighty = 1.5;
+        sideGbc.ipadx = 15;
+        sideGbc.ipady = 15;
         sideGbc.fill = GridBagConstraints.BOTH;
         sidePanel.add(optionsPanel, sideGbc);
 
-        optionsPanel.setLayout(new BorderLayout(0, 0));
-        optionsPanel.add(new JButton("Options panel"));
+        optionsPanel.setLayout(new BoxLayout(optionsPanel, BoxLayout.PAGE_AXIS));
+        optionsPanel.setBorder(BorderFactory.createEmptyBorder(15, 15, 0, 100));
+        JButton logout = new JButton(new ImageIcon(logoutImage));
+        JButton settings = new JButton(new ImageIcon(settingsImage));
+        JButton userPic = new JButton(new ImageIcon(userImage));
+        userPic.setEnabled(false);
+
+        optionsPanel.add(logout);
+        optionsPanel.add(settings);
+        optionsPanel.add(userPic);
 
         /* conversations panel */
         JPanel conversationsPanel = new JPanel();
@@ -45,9 +69,12 @@ public class Pan extends JPanel {
         sideGbc.weightx = 1.0;
         sideGbc.weighty = 1.0;
         sideGbc.fill = GridBagConstraints.BOTH;
+        sideGbc.insets = new Insets(0, 15, 0, 15);
+        sideGbc.ipadx = 15;
+        sideGbc.ipady = 15;
         sidePanel.add(conversationsPanel, sideGbc);
-
         conversationsPanel.setLayout(new BorderLayout());
+        conversationsPanel.setBorder(BorderFactory.createEmptyBorder(0, 0, 30 ,0));
         String[] contacts = {"Camille Wagner",
                 "Renee de Guyon",
                 "Christine Begue",
@@ -57,15 +84,41 @@ public class Pan extends JPanel {
                 "Xavier-Jacques Courtois",
                 "Victor de Morvan",
                 "Antoine Vincent-Rousset",
-                "Lucy-Michelle Teixeira",};
+                "Lucy-Michelle Teixeira",
+                "Camille Wagner",
+                "Renee de Guyon",
+                "Christine Begue",
+                "Jules Lesage",
+                "Noel Lebrun",
+                "William du Guillon",
+                "Xavier-Jacques Courtois",
+                "Victor de Morvan",
+                "Antoine Vincent-Rousset",
+                "Lucy-Michelle Teixeira",
+                "Camille Wagner",
+                "Renee de Guyon",
+                "Christine Begue",
+                "Jules Lesage",
+                "Noel Lebrun",
+                "William du Guillon",
+                "Xavier-Jacques Courtois",
+                "Victor de Morvan",
+                "Antoine Vincent-Rousset",
+                "Lucy-Michelle Teixeira"};
         JList<String[]> listeContacts = new JList(contacts);
         listeContacts.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
         listeContacts.setLayoutOrientation(JList.VERTICAL);
-        listeContacts.setVisibleRowCount(-1);
+        listeContacts.setVisibleRowCount(12);
         listeContacts.setFont(new Font("Arial", Font.BOLD, 20));
-        listeContacts.setFixedCellHeight(50);
+        listeContacts.setFixedCellHeight(40);
+        JScrollPane scrollPane = new JScrollPane(listeContacts);
+        conversationsPanel.add(scrollPane, BorderLayout.CENTER);
+        JPanel conversationButtonsPanel = new JPanel();
+        conversationsPanel.add(conversationButtonsPanel, BorderLayout.SOUTH);
+        conversationButtonsPanel.setLayout(new BorderLayout());
+        conversationButtonsPanel.add(new JButton("+"), BorderLayout.EAST);
+        conversationButtonsPanel.setBorder(BorderFactory.createEmptyBorder(15, 0, 0, 0));
 
-        conversationsPanel.add(listeContacts);
 
         /* message panel */
         JPanel messagePanel = new JPanel();
@@ -76,22 +129,17 @@ public class Pan extends JPanel {
         mainGbc.fill = GridBagConstraints.BOTH;
         this.add(messagePanel, mainGbc);
 
-        messagePanel.setLayout(new BorderLayout());
-
-        /* writing panel */
-        JPanel writingPanel = new JPanel();
-        messagePanel.add(writingPanel, BorderLayout.SOUTH);
-        writingPanel.setLayout(new BorderLayout());
-
-        JTextField textField = new JTextField("Message");
-        writingPanel.add(textField, BorderLayout.CENTER);
-
-        JButton sendButton = new JButton("Send");
-        writingPanel.add(sendButton, BorderLayout.EAST);
+        messagePanel.setLayout(new GridBagLayout());
+        GridBagConstraints msgGbc = new GridBagConstraints();
 
         /* chat panel */
         JTabbedPane chatPanel = new JTabbedPane();
-        messagePanel.add(chatPanel, BorderLayout.CENTER);
+        msgGbc.gridx = 0;
+        msgGbc.gridy = 0;
+        msgGbc.weightx = 1.0;
+        msgGbc.weighty = 20.0;
+        msgGbc.fill = GridBagConstraints.BOTH;
+        messagePanel.add(chatPanel, msgGbc);
 
         // placeholder panels
         JPanel[] panels = new JPanel[5];
@@ -99,5 +147,37 @@ public class Pan extends JPanel {
             panels[i] = new JPanel();
             chatPanel.addTab("Onglet " + i, panels[i]);
         }
+
+        /* writing panel */
+        JPanel writingPanel = new JPanel();
+        msgGbc.gridx = 0;
+        msgGbc.gridy = 1;
+        msgGbc.weightx = 1.0;
+        msgGbc.weighty = 1.0;
+        msgGbc.fill = GridBagConstraints.HORIZONTAL;
+        messagePanel.add(writingPanel, msgGbc);
+        writingPanel.setLayout(new GridBagLayout());
+        GridBagConstraints wrtGbc = new GridBagConstraints();
+        JTextField textField = new JTextField("Message");
+        wrtGbc.gridx = 0;
+        wrtGbc.gridy = 0;
+        wrtGbc.weightx = 15.0;
+        wrtGbc.weighty = 1.0;
+        wrtGbc.fill = GridBagConstraints.BOTH;
+        wrtGbc.insets = new Insets(0, 15, 0, 15);
+        wrtGbc.ipady = 15;
+        wrtGbc.ipadx = 15;
+        writingPanel.add(textField, wrtGbc);
+
+        JButton sendButton = new JButton("Send");
+        wrtGbc.gridx = 1;
+        wrtGbc.gridy = 0;
+        wrtGbc.weightx = 1.0;
+        wrtGbc.weighty = 1.0;
+        wrtGbc.fill = GridBagConstraints.BOTH;
+        wrtGbc.insets = new Insets(0, 15, 0, 15);
+        wrtGbc.ipady = 15;
+        wrtGbc.ipadx = 15;
+        writingPanel.add(sendButton, wrtGbc);
     }
 }
