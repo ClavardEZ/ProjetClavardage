d4794c32755dd12b2af47ae267c24cbf77970535
Noecv <chauveanpro@gmail.com>
2021-11-22 17:57:20 +0100

    TCP v0.2

--- app/src/main/java/ProjetClavardage/Model/MessageThreadManager.java
+++ app/src/main/java/ProjetClavardage/Model/MessageThreadManager.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
 /**
  * Classe gérant l'envoi et la reception de messages, et gère les conversations ainsi que la base de données
  */
-public class MessageThreadManager {
+public class MessageThreadManager extends Thread {
 
     private ArrayList<UserInConv> userInConvs;
 
@@ -23,4 +23,30 @@ public class MessageThreadManager {
     public void Send(Message msg) {
     }
 
+    public void run(){
+        int nb_conv_max = 50;
+        int nb_connection = 0;
+        Socket sock;
+        Conversation[] conversations = new Conversation[50];
+        for (int i = 0; i < nb_conv_max; i++) {
+            conversations[i] = null;
+        }
+
+        ServerSocket servsock = null;
+        try {
+            servsock = new ServerSocket(9000,2, InetAddress.getLocalHost());
+
+            while (nb_connection<=nb_conv_max) {
+                sock = servsock.accept();
+                int i = 0;
+                while (conversations[i] != null) {  //On cherche le premier élément libre de conversations
+                    i++;
+                }
+                conversations[i] = new Conversation("Coversation #" + i, servsock, sock);
+                nb_connection++;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
